72d23e5698db8832f639430e9add7b88729749b7
cooperrunyan <cooperrunyanspam@gmail.com>
2023-01-10 11:22:39 -0700

    Add logo to nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,12 +3,17 @@ import style from './Nav.module.scss';
 export const Nav: React.FC = () => {
   return (
     <nav className={style.Nav}>
-      <a href="/" className={style.link}>
-        Overview
+      <a href="/">
+        <img src="/logo.svg" />
       </a>
-      <a href="/#schedule" className={style.contact}>
-        Contact Sales
-      </a>
+      <div className={style.links}>
+        <a href="/" className={style.link}>
+          Overview
+        </a>
+        <a href="/#schedule" className={style.contact}>
+          Contact Sales
+        </a>
+      </div>
     </nav>
   );
 };
