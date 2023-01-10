fd2f71813ed870a622f3d254f42faba16fa60141
cooperrunyan <cooperrunyanspam@gmail.com>
2023-01-10 11:14:43 -0700

    resolve

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,25 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+// @ts-ignore
+import useSystemTheme from 'react-use-system-theme';
+
 import '../style/base.scss';
 import '../style/eula.scss';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const theme = (useSystemTheme && useSystemTheme('dark')) || 'light';
+
   return (
     <>
       <Head>
         <title>Market Drive</title>
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href={`/${theme || 'light'}/favicon.svg`}
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Component {...pageProps} />
     </>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,12 @@ const Document: React.FC = () => {
     <Html>
       <Head>
         <meta charSet="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
